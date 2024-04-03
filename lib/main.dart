9330a9a95928faf9c46e6ededb6f13f9d2f58f72
rlaalsvkdl <police0418@gmail.com>
2024-04-03 18:01:50 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('AppBar'),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+        Container(
+          width: 50,
+          height: 50,
+          color: Colors.orange,
+        ),
+          ],
+        ),
+      ),
+    );
+  }
+}
